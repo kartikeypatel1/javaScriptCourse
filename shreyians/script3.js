cceebf4dd13f17c6465962d63594d8216677c554
kartikeypatel1 <kp0543985@gmail.com>
2026-02-11 00:28:49 +0530

    revise the hoisting

---
+++ shreyians/script3.js
@@ -0,0 +1,18 @@
+//scope -functional scope, global scope and block scope
+
+//function scope- function ke andar hi use ho skti hai
+//global scope- poore code mein kahi bhi youse ho skti hai
+//block scope- curly braces mein hi use ho skti hai
+
+
+// function abcd(){
+//     var a=3;// that is fucntional scoped
+// }
+
+// {
+//     var a=90;//that is called blocked scoped
+// }
+
+// var b=9; //that is called the global scoped
+
+
